3f6857ae56d6ce4072fc118c670186ae8ce567fa
Valentyn <39245613+valik17666@users.noreply.github.com>
2020-09-23 20:54:53 +0300

    added bubble sorting implementation

---
+++ Сортування пузирьком/main.py
@@ -0,0 +1,14 @@
+nums = [2, 5, 6, 6, 2, 1, 6, 6, 9, 7]
+
+print(nums)
+
+swaps = True
+while swaps:
+    swaps = False
+
+    for j in range(len(nums) - 1):
+        if nums[j] > nums[j + 1]:
+            swaps = True
+            nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+            print(nums)
